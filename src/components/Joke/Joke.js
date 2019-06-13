617f36c03a5c9b249882f29e151a35db6c13489a
Eva Maria Ramirez <eramirez@MacBook-Pro-de-Saul.local>
2019-06-12 21:59:42 -0700

    Project from context-demo

--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -1,39 +1,18 @@
 /** @jsx jsx */
 import React from "react";
+import { compose } from 'redux';
 import { jsx } from "@emotion/core";
 import styles from "./styles";
-import Categories from "../Categories";
 import notFunnyImage from "../../images/notFunny.png";
 import funnyImage from "../../images/funny.jpg";
-import queryString from "query-string";
+import withJokeHOC from "../withJokeHOC";
 
-const Joke = () => {
-  const [joke, setJoke] = React.useState(null);
-  const [loading, setLoading] = React.useState(false);
-  const [category, setCategory] = React.useState(null);
-
-  const fetchNewJoke = async () => {
-    setLoading(true);
-    const response = await fetch(
-      `https://api.chucknorris.io/jokes/random?${
-        category
-          ? queryString.stringify({
-              category
-            })
-          : ""
-      }`
-    );
-    const joke = await response.json();
-    setJoke(joke.value);
-    setLoading(false);
-  };
-
+const Joke = ({ fetchNewJoke, joke, loading, name }) => {
   return (
     <div css={styles}>
       <div className="newJokeBtn">
-        <button onClick={fetchNewJoke}>Fetch it you coward!</button>
+        <button onClick={fetchNewJoke}>{`Fetch it you ${name}!`}</button>
       </div>
-      <Categories category={category} setCategory={setCategory} />
       <div className="joke">
         {loading ? (
           <span role="img" aria-label="waiting">
@@ -50,4 +29,6 @@ const Joke = () => {
   );
 };
 
-export default Joke;
+export default compose(
+  withJokeHOC('Flavio'),
+)(Joke)
